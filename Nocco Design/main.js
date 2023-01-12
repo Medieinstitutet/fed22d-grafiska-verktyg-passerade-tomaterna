095110f5b4985a48e88e81054baff195e92bdddd
Sebastian Post <sebastianpost@protonmail.com>
2023-01-12 10:49:58 +0100

    Correcting some bugs

--- Nocco Design/main.js
+++ Nocco Design/main.js
@@ -1,6 +1,7 @@
 import './style.css'
 import '/Nocco Design/array.js'
 
+
 document.querySelector('#slideShow').innerHTML = `
 <div class="imgSlider">
 <img src="Assets/Slideshow/cola.png"></img><br>
@@ -12,33 +13,36 @@ arrow_right
 </button>
 </div>
 `
-
+/*
+const logo = document.getElementById('logo');
+const noccoLogo = document.getElementById('noccoLogo');
+const slideShow = document.getElementById('slideShow');*/
+const menuBtn = document.getElementById('menuBtn')
+const closeBtn = document.getElementById('closeBtn')
+const arrowLeft = document.getElementById('arrow_left')
+const arrowRight = document.getElementById('arrow_right')
 document.querySelector(".noccoLogo").classList.remove("hidden");
 document.querySelector(".logo").classList.remove("hidden");
-
-
-const menuBtn = document.getElementById('menuBtn')
 menuBtn.addEventListener("click", toggleClass)
-
-const closeBtn = document.getElementById('closeBtn')
 closeBtn.addEventListener("click", toggleClass)
-
-const arrowLeft = document.getElementById('arrow_left')
 arrowLeft.addEventListener("click", togglePics)
-
-const arrowRight = document.getElementById('arrow_right')
 arrowRight.addEventListener("click", togglePics)
 
+
 function togglePics() {
-  console.log('Hej hej')
+  console.log('Kuken')
 }
 
 function toggleClass(){
   console.log('hej');
-  document.querySelector("#slideshow").classList.toggle("hidden");
-  document.querySelector(".noccoLogo").classList.toggle("hidden");
-  document.querySelector(".logo").classList.toggle("hidden");
-  hamburger.classList.toggle('hidden');
+
   menuBtn.classList.toggle('hidden');
   closeBtn.classList.toggle('show');
+  hamburger.classList.toggle('hidden');
+  hamburger.style.zIndex="100"
+  /*
+  slideShow.classList.toggle("hidden");
+  logo.classList.toggle("hidden");
+  noccoLogo.classList.toggle("hidden");*/
+
 }
