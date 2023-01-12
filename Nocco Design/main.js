8a69f501b65005398926a43c2565a14c15b0f871
Abdullah Mahmoud <89324398+KUNG3N@users.noreply.github.com>
2023-01-12 05:30:54 +0100

    sorterade all kod så den är readeble, fixade loggan till nav baren och skrev js så att den försvinner när man öppnar och adderad een till som jag ska style rätt.

--- Nocco Design/main.js
+++ Nocco Design/main.js
@@ -13,12 +13,19 @@ arrow_right
 </div>
 `
 
+document.querySelector(".noccoLogo").classList.remove("hidden");
+document.querySelector(".logo").classList.remove("hidden");
+
+
 const menuBtn = document.getElementById('menuBtn')
 menuBtn.addEventListener("click", toggleClass)
+
 const closeBtn = document.getElementById('closeBtn')
 closeBtn.addEventListener("click", toggleClass)
+
 const arrowLeft = document.getElementById('arrow_left')
 arrowLeft.addEventListener("click", togglePics)
+
 const arrowRight = document.getElementById('arrow_right')
 arrowRight.addEventListener("click", togglePics)
 
@@ -28,7 +35,10 @@ function togglePics() {
 
 function toggleClass(){
   console.log('hej');
+  document.querySelector("#slideshow").classList.toggle("hidden");
+  document.querySelector(".noccoLogo").classList.toggle("hidden");
+  document.querySelector(".logo").classList.toggle("hidden");
   hamburger.classList.toggle('hidden');
   menuBtn.classList.toggle('hidden');
-  closeBtn.classList.toggle('visa');
+  closeBtn.classList.toggle('show');
 }
