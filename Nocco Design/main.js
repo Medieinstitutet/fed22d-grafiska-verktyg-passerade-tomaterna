b56eea989364ba448b5a8dae68d4ad4306fe933f
Abdullah Mahmoud <89324398+KUNG3N@users.noreply.github.com>
2023-01-14 04:27:34 +0100

    gjorde ganksa mycket, 1-sorterade om koden i html och css så att den är i logisk ordning.
    2-fixade menys så att all text och bilden är centrerade och att det finns linjer mellan de.
    
    3-gjorde så att js gömmer hela sidan när menyn är öppen enligt design och att menys tar helsten av sidan i mobil läge.
    
    4-stylade menyknapparna efter figam färger.
    
    and yeah, jag tror det var detjag gjorde, om jag kommer på nåt anna så kommer jag pucha det senare

--- Nocco Design/main.js
+++ Nocco Design/main.js
@@ -1,28 +1,34 @@
 import './style.css'
 import '/Nocco Design/array.js'
 
+// i transfered all the code inside th ehtml cuz that way everything works better
+// document.querySelector('#slideShow').innerHTML =
+// `
+// <div class="imgSlider">
+// <img src="Assets/Slideshow/cola.png"></img><br>
+// <button class="material-symbols-outlined" id="arrow_left">
+// arrow_left
+// </button>
+// <button class="material-symbols-outlined" id="arrow_right">
+// arrow_right
+// </button>
+// </div>
+// `
 
-document.querySelector('#slideShow').innerHTML = `
-<div class="imgSlider">
-<img src="Assets/Slideshow/cola.png"></img><br>
-<button class="material-symbols-outlined" id="arrow_left">
-arrow_left
-</button>
-<button class="material-symbols-outlined" id="arrow_right">
-arrow_right
-</button>
-</div>
-`
-/*
+
 const logo = document.getElementById('logo');
 const noccoLogo = document.getElementById('noccoLogo');
-const slideShow = document.getElementById('slideShow');*/
+// const slideShow = document.getElementById('slideShow');
 const menuBtn = document.getElementById('menuBtn')
 const closeBtn = document.getElementById('closeBtn')
 const arrowLeft = document.getElementById('arrow_left')
 const arrowRight = document.getElementById('arrow_right')
+const hamburger = document.getElementById('hamburger')
+const content = document.getElementById('everythingContainer')
+
 document.querySelector(".noccoLogo").classList.remove("hidden");
 document.querySelector(".logo").classList.remove("hidden");
+
 menuBtn.addEventListener("click", toggleClass)
 closeBtn.addEventListener("click", toggleClass)
 arrowLeft.addEventListener("click", togglePics)
@@ -36,13 +42,14 @@ function togglePics() {
 function toggleClass(){
   console.log('hej');
 
+  hamburger.classList.toggle('hidden');
+
   menuBtn.classList.toggle('hidden');
   closeBtn.classList.toggle('show');
-  hamburger.classList.toggle('hidden');
-  hamburger.style.zIndex="100"
-  /*
-  slideShow.classList.toggle("hidden");
-  logo.classList.toggle("hidden");
-  noccoLogo.classList.toggle("hidden");*/
+  
+  content.classList.toggle('hidden');
+  document.querySelector(".nav").classList.toggle("hidden");
 
+  logo.classList.toggle("hidden");
+  noccoLogo.classList.toggle("hidden");
 }
