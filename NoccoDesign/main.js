53923d3929c4b94e056c43969cd6f895e213a154
FilipHedlen <114926854+FilipHedlen@users.noreply.github.com>
2023-01-20 01:57:08 +0100

    Desktop assortment rendering, needs some touch-ups

--- NoccoDesign/main.js
+++ NoccoDesign/main.js
@@ -74,3 +74,104 @@ if ($cookiesBanner) {
     $cookiesBanner.remove();
   });
 }
+
+// Changing assortmentpage to render in desktop viewport
+const mediaQuery = window.matchMedia("(min-width: 1025px)");
+
+if (mediaQuery.matches) { // Check if the viewport is 1025px or bigger
+  
+  let BCAABtn = document.createElement("button"); // Creating buttons which will appear in the buttonContainer Div
+  BCAABtn.id = "BCAABtn";
+  BCAABtn.className = "bcaabtn";
+  BCAABtn.innerHTML = "BCAA";
+  document.body.appendChild(BCAABtn);
+
+  let KoffeinfriBtn = document.createElement("button");
+  KoffeinfriBtn.id = "KoffeinfriBtn";
+  KoffeinfriBtn.className = "caffeinfreebtn";
+  KoffeinfriBtn.innerHTML = "Koffeinfri";
+  document.body.appendChild(KoffeinfriBtn);
+
+  let PWOBtn = document.createElement("button");
+  PWOBtn.id = "PWOBtn";
+  PWOBtn.className = "pwobtn";
+  PWOBtn.innerHTML = "PWO";
+  document.body.appendChild(PWOBtn);
+
+  let FocusBtn = document.createElement("button");
+  FocusBtn.id = "FocusBtn";
+  FocusBtn.className = "focusbtn";
+  FocusBtn.innerHTML = "Focus";
+  document.body.appendChild(FocusBtn);
+
+  
+  const bcaaArticles = document.querySelectorAll(".bcaa"); // Articles from HTML
+  const caffeinfreeArticles = document.querySelectorAll(".caffeinfree");
+  const pwoArticles = document.querySelectorAll(".pwo");
+  const focusArticles = document.querySelectorAll(".focus");
+
+  
+  BCAABtn.addEventListener("click", function() { // Add event listeners to the buttons
+    for (let i = 0; i < bcaaArticles.length; i++) {
+      bcaaArticles[i].style.display = "block";
+    }
+    for (let i = 0; i < caffeinfreeArticles.length; i++) {
+      caffeinfreeArticles[i].style.display = "none";
+    }
+    for (let i = 0; i < pwoArticles.length; i++) {
+      pwoArticles[i].style.display = "none";
+    }
+    for (let i = 0; i < focusArticles.length; i++) {
+      focusArticles[i].style.display = "none";
+    }
+  });
+    KoffeinfriBtn.addEventListener("click", function() {
+      for (let i = 0; i < bcaaArticles.length; i++) {
+        bcaaArticles[i].style.display = "none";
+      }
+      for (let i = 0; i < caffeinfreeArticles.length; i++) {
+        caffeinfreeArticles[i].style.display = "block";
+      }
+      for (let i = 0; i < pwoArticles.length; i++) {
+          pwoArticles[i].style.display = "none";
+        }
+      for (let i = 0; i < focusArticles.length; i++) {
+          focusArticles[i].style.display = "none";
+        }
+      });
+      PWOBtn.addEventListener("click", function() {
+        for (let i = 0; i < bcaaArticles.length; i++) {
+          bcaaArticles[i].style.display = "none";
+        }
+        for (let i = 0; i < caffeinfreeArticles.length; i++) {
+          caffeinfreeArticles[i].style.display = "none";
+        }
+        for (let i = 0; i < pwoArticles.length; i++) {
+          pwoArticles[i].style.display = "block";
+        }
+        for (let i = 0; i < focusArticles.length; i++) {
+          focusArticles[i].style.display = "none";
+        }
+      });
+      FocusBtn.addEventListener("click", function() {
+        for (let i = 0; i < bcaaArticles.length; i++) {
+          bcaaArticles[i].style.display = "none";
+        }
+        for (let i = 0; i < caffeinfreeArticles.length; i++) {
+          caffeinfreeArticles[i].style.display = "none";
+        }
+        for (let i = 0; i < pwoArticles.length; i++) {
+          pwoArticles[i].style.display = "none";
+        }
+        for (let i = 0; i < focusArticles.length; i++) {
+          focusArticles[i].style.display = "block";
+        }
+      });
+}
+
+
+const buttonContainer = document.querySelector(".buttoncontainer"); // Container with the different type of drinks
+buttonContainer.appendChild(BCAABtn);
+buttonContainer.appendChild(KoffeinfriBtn);
+buttonContainer.appendChild(PWOBtn);
+buttonContainer.appendChild(FocusBtn);
